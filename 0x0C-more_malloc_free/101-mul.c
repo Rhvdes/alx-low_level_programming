516fce688022a3ec4862dce7d0c0f316021735a1
Rhvdes <awwal.sulaiman63@gmail.com>
2022-11-11 17:30:07 -0800

    Write a program that multiplies two positive numbers fixed.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -88,7 +88,8 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2); _is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
