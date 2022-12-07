e6dc3ed4ec56e99ce8d40cbf88fce69612786e89
Awwal Sulaiman <awwal.sulaiman63@gmail.com>
2022-12-06 16:48:08 -0800

    fixed: Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,36 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	/* Return 0 if filename is NULL*/
 	if (!filename)
-	{
 		return (0);
-	}
 
-	/* Open the file for reading*/
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
-	{
 		return (0);
-	}
 
-	/* Allocate a buffer to hold the read data*/
-	buffer = malloc(letters);
-	if (!buffer)
-	{
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	/* Read up to "letters" number of bytes from the file*/
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	/* Write the read data to standard output*/
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	/* Free the buffer and close the file*/
-	free(buffer);
 	close(fd);
 
-	/* Return the number of bytes read and written*/
-	return (bytes_read);
+	free(buf);
+
+	return (nwr);
 }
